84eb82614a4405a6cd6ed3cee8933cd71ed41ccf
anwar8983ali <158761862+anwar8983ali@users.noreply.github.com>
2026-03-31 12:52:39 +0530

    Update API endpoint for fetching orders

--- deshboard/src/components/Orders.js
+++ deshboard/src/components/Orders.js
@@ -11,7 +11,7 @@ fetchOrders();
 
 const fetchOrders=async()=>{
 try{
-const res=await axios.get("http://localhost:3002/allOrders");
+const res=await axios.get("https://investo-hdsy.onrender.com/allOrders");
 setOrders(res.data);
 }catch(err){
 console.error("Error fetching orders:",err);
