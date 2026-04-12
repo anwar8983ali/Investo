965439a9114acf5ab370c92b737943a61b9ab2f3
anwar8983ali <158761862+anwar8983ali@users.noreply.github.com>
2026-04-12 15:00:01 +0530

    Update API endpoint for fetching all holdings

--- deshboard/src/components/Holdings.js
+++ deshboard/src/components/Holdings.js
@@ -8,7 +8,7 @@ const Holdings = () => {
   const [allHoldings, setAllHoldings] = useState([]);
 
   useEffect(() => {
-    axios.get("https://investo-hdsy.onrender.com/allHoldings").then((res) => {
+    axios.get("https://investo-hdsy.onrender.com").then((res) => {
       // console.log(res.data);
       setAllHoldings(res.data);
     });
