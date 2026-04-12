d82402dcb4661ae675862706ebb48c722b3eb286
anwar8983ali <158761862+anwar8983ali@users.noreply.github.com>
2026-04-12 15:16:27 +0530

    Update Positions.js

--- deshboard/src/components/Positions.js
+++ deshboard/src/components/Positions.js
@@ -5,7 +5,7 @@ const Positions = () => {
   const [allPositions, setAllPositions] = useState([]);
 
   useEffect(() => {
-    axios.get("http://https://investo-hdsy.onrender.com/allpositions").then((res) => {
+    axios.get("http://https://investo-hdsy.onrender.com/allPositions").then((res) => {
       // console.log(res.data);
       setAllPositions(res.data);
     });
