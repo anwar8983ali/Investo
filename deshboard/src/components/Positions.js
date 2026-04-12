93aae83859a2c5d3e833dbcd6197e54f446532ca
anwar8983ali <158761862+anwar8983ali@users.noreply.github.com>
2026-04-12 17:07:31 +0530

    Fix axios URL by removing extra 'http://'

--- deshboard/src/components/Positions.js
+++ deshboard/src/components/Positions.js
@@ -5,7 +5,7 @@ const Positions = () => {
   const [allPositions, setAllPositions] = useState([]);
 
   useEffect(() => {
-    axios.get("http://https://investo-hdsy.onrender.com/allPositions").then((res) => {
+    axios.get("https://investo-hdsy.onrender.com/allPositions").then((res) => {
       // console.log(res.data);
       setAllPositions(res.data);
     });
